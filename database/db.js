a21cb8a0fd0c0038d12fba484c47171dc9940e45
Samuel Christensen <samachr@gmail.com>
2015-05-30 21:51:33 -0600

    broken database code fix

--- database/db.js
+++ database/db.js
@@ -1,6 +1,7 @@
 var sqlite3 = require('sqlite3').verbose();
 var dbdir = process.env.OPENSHIFT_DATA_DIR || 'database/';
 var db = new sqlite3.Database(dbdir + 'inventory.db');
+var example = false;
 
 db.run("CREATE TABLE IF NOT EXISTS productInventory (id INTEGER PRIMARY KEY, price REAL, picture INT, name TEXT, stock INT, brand text)");
 db.run("CREATE TABLE IF NOT EXISTS brands (id INTEGER PRIMARY KEY, brand TEXT)");
